7c5a6d5b400633bc570c1097e3ab7e29ac3c5cad
Hugo Camacho Romero <hugo@romero.com>
2025-05-10 21:14:21 +0100

    shop

--- shopping-tracker/models.py
+++ shopping-tracker/models.py
@@ -3,3 +3,12 @@ class Category(db.Model):
     name = db.Column(db.String(100), nullable=False)
 
     items = db.relationship('Item', backref='category', lazy=True)
+
+
+    class Shop(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(100), nullable=True)
+
+    items = db.relationship('Item', backref='shop', lazy=True)
+
