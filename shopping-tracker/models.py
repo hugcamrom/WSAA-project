a9148b6e60f4f8d9f5cbe60d19816706cc3bde18
Hugo Camacho Romero <hugo@romero.com>
2025-05-10 21:18:30 +0100

    indentation

--- shopping-tracker/models.py
+++ shopping-tracker/models.py
@@ -1,9 +1,10 @@
+from app import db
+
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
 
     items = db.relationship('Item', backref='category', lazy=True)
-
 
 class Shop(db.Model):
     id = db.Column(db.Integer, primary_key=True)
